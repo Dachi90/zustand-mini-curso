fa3901da889fea6ead9e9f22d96f73b43e913a3a
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-09-17 21:06:39 +0200

    seccion 1

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,53 +1,69 @@
 import { WhiteCard } from '../../components';
+import { useBearStore } from '../../stores';
 
 export const BearPage = () => {
-  return (
-    <>
-      <h1>Contador de Osos</h1>
-      <p>Manejo de estado simple de Zustand</p>
-      <hr />
+	return (
+		<>
+			<h1>Contador de Osos</h1>
+			<p>Manejo de estado simple de Zustand</p>
+			<hr />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+				<BlackBears />
 
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
+				<PolarBears />
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
+				<PandaBears />
+			</div>
+		</>
+	);
+};
 
-        </WhiteCard>
+export const BlackBears = () => {
+	const blackBears = useBearStore((state) => state.blackBears);
+	const increaseBlackBears = useBearStore((state) => state.increaseBlackBears);
+	return (
+		<WhiteCard centered>
+			<h2>Osos Negros</h2>
 
-        <WhiteCard centered>
-          <h2>Osos Polares</h2>
+			<div className='flex flex-col md:flex-row'>
+				<button onClick={() => increaseBlackBears(+1)}> +1</button>
+				<span className='text-3xl mx-2 lg:mx-10'> {blackBears} </span>
+				<button onClick={() => increaseBlackBears(-1)}>-1</button>
+			</div>
+		</WhiteCard>
+	);
+};
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
+export const PolarBears = () => {
+	const polarBears = useBearStore((state) => state.polarBears);
+	const increasePolarBears = useBearStore((state) => state.increasePolarBears);
 
-          </div>
+	return (
+		<WhiteCard centered>
+			<h2>Osos Polares</h2>
 
-        </WhiteCard>
+			<div className='flex flex-col md:flex-row'>
+				<button onClick={() => increasePolarBears(+1)}> +1</button>
+				<span className='text-3xl mx-2 lg:mx-10'> {polarBears} </span>
+				<button onClick={() => increasePolarBears(-1)}>-1</button>
+			</div>
+		</WhiteCard>
+	);
+};
 
-        <WhiteCard centered>
-          <h2>Osos Pandas</h2>
+export const PandaBears = () => {
+	const pandaBears = useBearStore((state) => state.pandaBears);
+	const increasePandaBears = useBearStore((state) => state.increasePandaBears);
+	return (
+		<WhiteCard centered>
+			<h2>Osos Pandas</h2>
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
-
-
-
-
-      </div>
-
-    </>
-  );
+			<div className='flex flex-col md:flex-row'>
+				<button onClick={() => increasePandaBears(+1)}> +1</button>
+				<span className='text-3xl mx-2 lg:mx-10'> {pandaBears} </span>
+				<button onClick={() => increasePandaBears(-1)}>-1</button>
+			</div>
+		</WhiteCard>
+	);
 };
